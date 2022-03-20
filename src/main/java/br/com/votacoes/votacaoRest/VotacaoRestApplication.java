a0c7a29dcc33f95f0f1d908eb942461c30b38816
WillianMSantos <william.marreiros2gmail.com>
2022-03-20 00:28:53 -0300

    Initial Commit

---
+++ src/main/java/br/com/votacoes/votacaoRest/VotacaoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.votacoes.votacaoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/votacoes/votacaoRest/VotacaoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votacoes.votacaoRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
