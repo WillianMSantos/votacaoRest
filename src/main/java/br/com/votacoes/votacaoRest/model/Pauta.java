7153a9ce2069cb4246a23c891d47d83a65dc9d1f
WillianMSantos <william.marreiros2gmail.com>
2022-03-20 23:35:34 -0300

    Cria atributo List<Associado>associados.

--- src/main/java/br/com/votacoes/votacaoRest/model/Pauta.java
+++ src/main/java/br/com/votacoes/votacaoRest/model/Pauta.java
@@ -4,10 +4,9 @@ package br.com.votacoes.votacaoRest.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.swing.text.StyleContext;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,6 +18,8 @@ public class Pauta {
     private String nome;
     private Integer qtdSim;
     private Integer qtdNao;
+    @OneToMany
+    private List<Associado>associados;
     private String resultado;
 
     public Pauta(){
